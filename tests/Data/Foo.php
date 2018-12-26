00c4a3d0dc01364364afbf8b056ae411a92d64d9
Dugi <me@dukagj.in>
2018-12-26 21:06:45 +0100

    Put the tests back up...

---
+++ tests/Data/Foo.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pouch\Tests\Data;
+
+use Pouch\Tests\Data\Sub\Baz;
+
+class Foo
+{
+    private $bar;
+
+    public function __construct(Bar $bar = null)
+    {
+        $this->bar = $bar;
+    }
+
+    public function testConstructor()
+    {
+        return 'FromFoo:'.$this->bar->barFunc1();
+    }
+
+    public function fooFunc1(Bar $bar, Baz $baz)
+    {
+        $barStr = $bar->barFunc1();
+        $bazStr = $baz->bazFunc1();
+
+        return "FooFunc1{$barStr}{$bazStr}";
+    }
+
+    public function simpleMethod()
+    {
+        return 'simpleMethod';
+    }
+}
---
+++ tests/Unit/ClassTreeTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Pouch\Tests\Unit;
+
+use Pouch\ClassTree;
+use Pouch\Tests\TestCase;
+use Pouch\Tests\Data\Bar;
+use Pouch\Tests\Data\Foo;
+use Pouch\Tests\Data\Sub\Baz;
+use Pouch\Exceptions\NamespaceNotFoundException;
+
+class ClassTreeTest extends TestCase
+{
+    public function test_fetching_namespace_list()
+    {
+        $classes = ClassTree::getClassesInNamespace('Pouch\Tests\Data');
+
+        $this->assertTrue(in_array(Foo::class, $classes));
+        $this->assertTrue(in_array(Bar::class, $classes));
+        $this->assertTrue(in_array(Baz::class, $classes));
+    }
+
+    public function test_fetching_with_invalid_namespace()
+    {
+        $this->expectException(NamespaceNotFoundException::class);
+
+        ClassTree::getClassesInNamespace('Foo\Bar');
+    }
+}
