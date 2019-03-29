4d784c91a30450cda5478106cf45cd7da893e1be
Dugi <me@dukagj.in>
2019-03-29 11:01:15 +0100

    Add some methods to the internalcontainer contract

--- src/Helpers/InternalContainer.php
+++ src/Helpers/InternalContainer.php
@@ -4,4 +4,17 @@ namespace Pouch\Helpers;
 
 interface InternalContainer
 {
+    /**
+     * Returns the class name of the container.
+     *
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * Returns the contents of the container.
+     *
+     * @return mixed
+     */
+    public function getContent();
 }
